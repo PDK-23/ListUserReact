6fa2f0e71580a3663e7fb4a474f0227fcbd2d118
Pham-Dang-Khoi <138744608+Pham-Dang-Khoi@users.noreply.github.com>
2024-12-31 23:16:38 +0700

    Update module, icon

--- src/Module/getListUser.js
+++ src/Module/getListUser.js
@@ -2,9 +2,6 @@ const mainAPI = "https://randomuser.me/api/?results=40&seed=";
 
 export const fetchUserList = async (seed = "dev") => {
     const response = await fetch(`${mainAPI}${seed}`);
-    // if (!response.ok) {
-    //     throw new Error(`HTTP error! Status: ${response.status}`);
-    // }
     const data = await response.json();
     return data.results;
 }

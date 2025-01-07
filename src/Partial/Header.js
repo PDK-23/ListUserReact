19da11936c2c438de24ab80d616f0e3577c3fec4
Pham-Dang-Khoi <138744608+Pham-Dang-Khoi@users.noreply.github.com>
2025-01-07 21:25:10 +0700

    xoa

--- src/Partial/Header.js
+++ src/Partial/Header.js
@@ -15,23 +15,7 @@ export default function Header({ search, setSearch }) {
       </div>
       <div>
         <div className="relative">
-          <input
-            type="search"
-            name="search"
-            className="peer block w-full border rounded-md px-3 pt-4 pb-2 text-sm text-gray-900 placeholder-transparent
-                       focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 border-gray-300"
-            placeholder="Search name"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <label
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10
-            origin-[0] bg-neutral-50 dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 
-            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-            peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-[10px]"
-          >
-            Search name
-          </label>
+          
           {/* <Input size="large" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/> */}
         </div>
       </div>

d115a3d07e9e43460246e2035d776e1c12073457
son1506 <169061311+son1506@users.noreply.github.com>
2025-01-07 21:35:06 +0700

    bug ne

--- src/Partial/Header.js
+++ src/Partial/Header.js
@@ -24,15 +24,8 @@ export default function Header({ search, setSearch }) {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
-          <label
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10
-            origin-[0] bg-neutral-50 dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 
-            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-            peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-[10px]"
-          >
-            Search name
-          </label>
-          <Input size="large" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+
+          <Input size="large" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
       </div>
     </div>
